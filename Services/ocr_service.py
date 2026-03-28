a2fd96a811c525dca0268410dfb6503c76a20fc8
Gudboy <ninadnimkar2021@gmail.com>
2026-03-28 22:33:58 +0530

    dropdown UI index fix

--- Services/ocr_service.py
+++ Services/ocr_service.py
@@ -1,7 +1,13 @@
 import pdfplumber
-
 from PIL import Image
 import io
+from openai import OpenAI
+from fastapi import FastAPI
+import base64
+from llm_service import client
+from prompt_builder import build_prompt
+from dotenv import load_dotenv
+load_dotenv()
 
 def extract_text(file_bytes: bytes, filename) -> str:
     extracted_text = ""
@@ -14,6 +20,26 @@ def extract_text(file_bytes: bytes, filename) -> str:
 
     #Image handeling
     else:
-        pass
+        base64_image = base64.b64decode(file_bytes).decode("utf-8")
 
-    return extracted_text
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{
+                "role": "user",
+                "content": [{
+                    "type": "text",
+                    "text":f"extract text"
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/png;base64,{base64_image}"}
+                }]
+            }],
+            max_tokens=1500,
+            temperature=0.3
+        )
+
+    return{
+        "input_type": "image",
+        "solution": response.choices[0].message.content
+    }
