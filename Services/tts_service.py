e3d1fd9f30080a2f29b23c02b775ae007eef5336
Gudboy <ninadnimkar2021@gmail.com>
2026-05-01 17:06:29 +0530

    cleaned tts code

--- Services/tts_service.py
+++ Services/tts_service.py
@@ -3,13 +3,19 @@ import os
 from dotenv import load_dotenv
 import base64
 from Services.prompt_builder import SPEAKER_BY_LANGAUGE
+
+#Load the api
 load_dotenv()
 
+#create the client
 client = SarvamAI(api_subscription_key = os.getenv("SARVAM_API_KEY"))
 
-def generate_audio(text: str, language: str):
-
+def generate_audio(text: str, language: str) -> bytes:
+    
+    #get speaker and language code for the selected language
+    #falls back to english is languiage not found
     config = SPEAKER_BY_LANGAUGE.get(language, SPEAKER_BY_LANGAUGE["hindi"])
+
 
     audio = client.text_to_speech.convert(
         target_language_code = config["target_language_code"],
@@ -20,8 +26,5 @@ def generate_audio(text: str, language: str):
 
     combined_audio = "".join(audio.audios)
     audio_bytes = base64.b64decode(combined_audio)
-
-    with open("output.wav", "wb") as f:
-        f.write(audio_bytes)
 
     return audio_bytes
